87a0611bdb67ddad144e171d213c1dbff694ae13
Andrew <amh11706@gmail.com>
2020-11-29 00:59:22 +0100

    add context

--- shutdown.go
+++ shutdown.go
@@ -1,13 +1,15 @@
 package shutdown
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
-var tasks = make([]func(), 0)
+var tasks = make([]func(context.Context), 0)
 var quit = make(chan os.Signal, 1)
 var Closing bool
 
@@ -15,18 +17,20 @@ func init() {
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 }
 
-func AddTask(f func()) {
+func AddTask(f func(context.Context)) {
 	if f != nil {
 		tasks = append(tasks, f)
 	}
 }
 
 func Watch() {
-	_ = <-quit
+	<-quit
 	log.Println("Shutting down")
 	Closing = true
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	for _, t := range tasks {
-		t()
+		t(ctx)
 	}
+	cancel()
 	log.Println("Shutdown complete")
 }
